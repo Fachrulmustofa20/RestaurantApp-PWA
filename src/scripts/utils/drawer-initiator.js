4c6e82d5508cfb7437e8c22e111e4c68156fe40a
Fachrulmustofa20 <fachrulmustofa100@gmail.com>
2021-06-16 11:21:22 +0700

    fix bug drawer in navbar

--- src/scripts/utils/drawer-initiator.js
+++ src/scripts/utils/drawer-initiator.js
@@ -1,22 +1,14 @@
 /* eslint-disable no-underscore-dangle */
 const DrawerInitiator = {
-  init({ button, drawer, content }) {
+  init({ button, drawer }) {
     button.addEventListener('click', (event) => {
       this._toggleDrawer(event, drawer, button);
-    });
-
-    content.addEventListener('click', (event) => {
-      this._closeDrawer(event, drawer);
     });
   },
   _toggleDrawer(event, drawer, button) {
     button.classList.toggle('open');
     drawer.classList.toggle('mobile-nav');
     event.preventDefault();
-  },
-  _closeDrawer(event, drawer) {
-    event.stopPropagation();
-    drawer.classList.remove('mobile-nav');
   },
 };
 
