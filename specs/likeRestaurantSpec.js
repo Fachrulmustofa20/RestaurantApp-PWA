667e740a41125f7cf551a27a7f8dba040616a94e
Fachrulmustofa20 <fachrulmustofa100@gmail.com>
2021-07-06 10:46:01 +0700

    add test should be able to like the restaurant

--- specs/likeRestaurantSpec.js
+++ specs/likeRestaurantSpec.js
@@ -23,6 +23,17 @@ describe('Liking or Adding a Restaurant', () => {
     expect(document.querySelector('[aria-label="unlike this restaurant]')).toBeFalsy();
   });
 
+  it('should be able to like the restaurant', async () => {
+    await createLikeButtonPresenterWithRestaurant({ id: 1 });
+
+    document.querySelector('#likeButton').dispatchEvent(new Event('click'));
+
+    const restaurant = await FavoriteRestaurantIdb.getResto(1);
+
+    expect(restaurant).toEqual({ id: 1 });
+    FavoriteRestaurantIdb.deleteResto(1);
+  });
+
   it('should not add a restaurant again when its already liked', async () => {
     await createLikeButtonPresenterWithRestaurant({ id: 1 });
 
